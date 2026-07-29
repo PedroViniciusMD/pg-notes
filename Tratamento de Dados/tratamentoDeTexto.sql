bbd1bf4aee3466e84f03898185f66f2bba9ae7b8
PedroViniciusMD <pedrodelgado583@gmail.com>
2026-07-29 12:59:33 -0300

    tratamento de texto

---
+++ Tratamento de Dados/tratamentoDeTexto.sql
@@ -0,0 +1,15 @@
+-- LOWER()
+-- UPPER()
+-- TRIM() -> remove espaços no início e no final da string
+-- REPLACE()
+
+SELECT UPPER('São Paulo') = 'SÃO PAULO'
+
+
+SELECT LOWER('São Paulo') = 'são paulo'
+
+
+SELECT TRIM('SÃO PAULO     ') = 'SÃO PAULO'
+
+
+SELECT REPLACE('SAO PAULO', 'SAO', 'SÃO') = 'SÃO PAULO'
